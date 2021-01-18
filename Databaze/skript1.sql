444ded0b63b56a63a82591c9b210de37c6279478
Tomas Exapos <klamota@email.cz>
2021-01-18 19:56:00 +0100

    Databaze_ukol

---
+++ Databaze/skript1.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE vysvedceni;
+
+USE vysvedceni;
+
+CREATE TABLE znamky (predmet CHAR(99), 1_pololeti INT, 2_pololeti INT);
+
+INSERT INTO znamky (predmet, 1_pololeti, 2_pololeti) VALUES ("Cestina", 3, 3);
+INSERT INTO znamky (predmet, 1_pololeti, 2_pololeti) VALUES ("Matika", 3, 2);
+INSERT INTO znamky (predmet, 1_pololeti, 2_pololeti) VALUES ("Prirodopis", 1, 2);
+INSERT INTO znamky (predmet, 1_pololeti, 2_pololeti) VALUES ("Dejepis", 2, 1);
+INSERT INTO znamky (predmet, 1_pololeti, 2_pololeti) VALUES ("Chemie", 1, 2); 
+INSERT INTO znamky (predmet, 1_pololeti, 2_pololeti) VALUES ("Informacni technologie", 1, 1); 
+INSERT INTO znamky (predmet, 1_pololeti, 2_pololeti) VALUES ("Zemepis", 1, 3); 
+INSERT INTO znamky (predmet, 1_pololeti, 2_pololeti) VALUES ("Anglicitna", 1, 1); 
